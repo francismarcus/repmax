78ce02ffe384ea71136ba2d27fe61f655dd1d9c6
Marcus Francis <marcusmagnusson@outlook.com>
2019-03-01 05:31:25 +0100

    Select reps

--- src/components/input.js
+++ src/components/input.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Input extends React.Component {
 
     state = { weight: '', reps: '' }
-   
+
     render() {
     return (
         <div>
@@ -16,14 +16,25 @@ class Input extends React.Component {
                 </form>
             </div>
             <div>
-                <form>
-                    <input 
-                    value={this.state.reps}
-                    onChange={e => this.setState({ reps: e.target.value })}>
-                    </input>
-                </form>
+            <select value={this.state.reps} onChange={e => this.setState({ reps: e.target.value })}>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
+            <option value='11'>11</option>
+            <option value='12'>12</option>
+            <option value='13'>13</option>
+            <option value='14'>14</option>
+            <option value='15'>15</option>
+                </select>
             </div>
-            1RM: {this.state.weight * (1 + this.state.reps / 30)}
+            1RM: {Math.round(this.state.weight * (1 + this.state.reps / 30))}
         </div>
     )}
 }
