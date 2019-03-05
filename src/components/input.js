f26ef3045092520d5278571ed691d96bb330c29a
Marcus Francis <marcusmagnusson@outlook.com>
2019-03-05 19:19:14 +0100

    Styled input.js

--- src/components/input.js
+++ src/components/input.js
@@ -1,43 +1,54 @@
 import React from 'react';
 
 class Input extends React.Component {
-
     state = { weight: '', reps: '' }
 
-    render() {
-    return (
-        <div>
-            <div>
-                <form>
-                    <input 
-                    value={this.state.weight}
-                    onChange={e => this.setState({ weight: e.target.value })}>
-                    </input>
-                </form>
+        render() {
+        return (
+          <div class="ui card">
+            <div class="content">
+            <h5>Select the amount of repetitions done and enter the weight used to calculate your 1RM</h5>
+            <div class="fields">
+            <div class="field">
+            <select class="ui fluid dropdown" placeholder="Select amunt of reps" value={this.state.reps} onChange={e =>
+                 this.setState({ reps: e.target.value })}>
+              <option value=""></option>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              <option value='6'>6</option>
+              <option value='7'>7</option>
+              <option value='8'>8</option>
+              <option value='9'>9</option>
+              <option value='10'>10</option>
+              <option value='11'>11</option>
+              <option value='12'>12</option>
+              <option value='13'>13</option>
+              <option value='14'>14</option>
+              <option value='15'>15</option>
+            </select>
+            </div>
+              <div class="field">
+              <form class="ui form">
+                <input
+                      type="number"
+                      placeholder="e.g. 120"
+                      value={this.state.weight}
+                      onChange={e => this.setState({ weight: e.target.value })}></input>
+              </form>
+              </div>
             </div>
             <div>
-            <select value={this.state.reps} onChange={e => this.setState({ reps: e.target.value })}>
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
-            <option value='6'>6</option>
-            <option value='7'>7</option>
-            <option value='8'>8</option>
-            <option value='9'>9</option>
-            <option value='10'>10</option>
-            <option value='11'>11</option>
-            <option value='12'>12</option>
-            <option value='13'>13</option>
-            <option value='14'>14</option>
-            <option value='15'>15</option>
-                </select>
+              <h4>
+                <br /> Your estimated 1RM is
+                <br /> {Math.round(this.state.weight * (1 + this.state.reps / 30))}
+              </h4>
             </div>
-            1RM: {Math.round(this.state.weight * (1 + this.state.reps / 30))}
-        </div>
-    )}
-}
+            </div>
+          </div>
+        )}
+    }
 
 export default Input;
-
